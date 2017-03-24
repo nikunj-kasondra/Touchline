a8e4b7bff80a43e7ca61be7726445867da4623a6
Rujal <Rujal@rujals-macbook-pro.local>
2017-03-24 22:22:41 +0530

    Initial Commit

---
+++ Touchline Bet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Touchline Bet
+//
+//  Created by Rujal on 3/24/17.
+//  Copyright © 2017 Rujal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
